807c271b6333789ba683706d0759c3c35201bde5
BART! <bart@bartweb.cz>
2019-11-06 16:54:11 +0200

    feat(filtering): better handling filter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { API_URL, COUNT } from './variables';
 import { GeocodingData, GeocodingOptions, GeocodingScope, GeocodingResult } from './geo.interface';
 
@@ -20,20 +20,20 @@ export class MapyCz {
 
   private filterData(data: GeocodingResult[], options: GeocodingOptions | undefined): GeocodingResult[] {
     if (options?.scope) {
-      let category = this.preparCategoryForFilter(options.scope);
+      // let category = this.preparCategoryForFilter(options.scope);
       return data
-        .filter((item) => (item.category === category))
-        .filter((item) => item.userData.source === 'muni');
+        .filter((item) => String(item.category).includes(String(options.scope)))
+        .filter((item) => item.userData.source === options.scope);
     } else {
       return data;
     }
   }
 
-  private preparCategoryForFilter(scope: GeocodingScope): string | undefined {
-    let category;
-    if (scope === 'muni') {
-      category = 'municipality_cz';
-    }
-    return category;
-  }
+  // private preparCategoryForFilter(scope: GeocodingScope): string | undefined {
+  //   let category;
+  //   if (scope === 'muni') {
+  //     category = 'municipality_cz';
+  //   }
+  //   return category;
+  // }
 }
