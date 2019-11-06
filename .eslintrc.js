67905236e7a5a67a061ebe13c5fa99092c3a8370
David Tyemnyák <davidtyemnyak@gmail.com>
2019-11-06 17:27:43 +0100

    Es lint (#1)
    * feat(eslint): init es lint
    
    * feat(example): eslint works on js files
    
    * chore(eslint):  fix typo
    
    * chore(eslint): convert to es6 module
    
    * refactor(test data): remove test data
    
    * chore(lint + prettier): update to  es modules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+export default {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['google', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+export default {
   singleQuote: true,
-  trailingComma: 'es5',
+  trailingComma: "es5",
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  arrowParens: "always",
+  bracketSpacing: true,
 };
