0097f1422410573e1ebdadab7c3ea9d2160a2e79
BART! <bart@bartweb.cz>
2019-11-06 17:33:07 +0200

    chore(eslint): convert to es6 module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true
+export default {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
   },
-  "extends": [
-    "google",
-    "plugin:prettier/recommended"
-  ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: ['google', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  "plugins": [
-    "@typescript-eslint",
-    "prettier"
-  ],
-  "rules": {
-    "prettier/prettier": "error"
-  },
-};
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
