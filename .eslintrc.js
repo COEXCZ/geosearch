17707bcf9d2f6a6c4cb8af5ab3e86beff0de4691
David Tyemnyák <davidtyemnyak@gmail.com>
2020-01-05 18:48:03 +0100

    Chore/hooks (#2)
    * feat(hooks): add pre commit hook
    
    * refactor(optional chaining): es lint does not support yet
    
    * chore(es lint): override google config
    
    * feat(hooks): add pre push hook
    
    * chore(hooks): make all hooks pre commit
    
    * chore(es lint): no unused variables typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
-export default {
+module.exports = {
   env: {
     browser: true,
     es6: true,
     node: true
   },
-  extends: ['google', 'plugin:prettier/recommended'],
+  extends: [
+    'google',
+    'plugin:prettier/recommended'
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -14,8 +17,20 @@ export default {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
   rules: {
+    // disable googles JSDoc
+    'require-jsdoc': 'off',
+    // no-unused vars for typescript config
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      'vars': 'all',
+      'args': 'after-used',
+      'ignoreRestSiblings': false
+    }],
     'prettier/prettier': 'error'
   }
 }
