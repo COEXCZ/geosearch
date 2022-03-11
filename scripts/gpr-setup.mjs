33758f228bebf9670451d7860b70756c51ed078c
BART! <bart@bartweb.cz>
2022-03-11 15:21:06 +0100

    chore(gpr): setup

--- scripts/gpr-setup.mjs
+++ scripts/gpr-setup.mjs
@@ -1,13 +1,17 @@
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 import distPackage from '../dist/package.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Modify package.json in dist folder
-const pkg: Record<string, any> = distPackage;
+const pkg = distPackage;
 
 pkg.publishConfig = {};
 pkg.name = '@COEXCZ/geosearch';
 pkg.publishConfig.registry = `https://npm.pkg.github.com`;
-
 writeFileSync(join(__dirname, '..', 'dist', 'package.json'), JSON.stringify(pkg, null, 2));
 console.log('File package.json modified:', pkg);
